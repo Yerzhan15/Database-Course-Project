30bad80dcd92ef4603d0d0c25334fee4eeb8f12b
Yerzhan <mademihanov.erzhan@gmail.com>
2016-11-12 13:31:37 +0600

    somekind understand get method varibles

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -37,4 +37,26 @@ public class Application extends Controller {
     	}
     	render(className + "/index.html", users);
     }
+
+    public static void show(Long id) throws SQLException {
+        List<User> users = new ArrayList<>();
+
+        // Database connection
+        Connection conn = DB.getConnection();
+        // query
+        String query = "select * FROM tbl_user";
+        // getting results
+        ResultSet rs = conn.createStatement().executeQuery(query);
+        // going through the results
+        while (rs.next()) {
+            User user = new User();
+            user.name = rs.getString("user_name");
+            user.email = "nope";
+            user.surname = "nope";
+            user.profileUrl = "https://yt3.ggpht.com/-v0soe-ievYE/AAAAAAAAAAI/AAAAAAAAAAA/OixOH_h84Po/s900-c-k-no-mo-rj-c0xffffff/photo.jpg";
+            users.add(user);
+        }
+        System.out.println(id);
+        render(className + "/index.html", users);
+    }
 }
--- app/models/User.java
+++ app/models/User.java
@@ -6,4 +6,16 @@ public class User {
 	public String name;
 	public String surname;
 	public String profileUrl;
+
+	// constructor
+	public User() {
+		
+	}
+	public User(int id, String email, String name, String surname, String profileUrl) {
+		this.id = id;
+		this.email = email;
+		this.name = name;
+		this.surname = surname;
+		this.profileUrl = profileUrl;
+	}
 }
